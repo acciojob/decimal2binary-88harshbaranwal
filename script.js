11222864814089c6094dc6dba8a333be91975c90
Harsh Baranwal <127023632+88harshbaranwal@users.noreply.github.com>
2023-10-03 14:03:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 function decimalToBinary(num) {
 //   //Write you code here
-// 	if(num===0){
-// 		return 0;
-// 	}
-// 	let binary ='';
-// 	while(num>0){
-// 		const rem = num%2;
-// 		binary += rem;
-// 		num = Math.floor(num/2);
-// 	}
-// 	return binary;
+	if(num===0){
+		return 0;
+	}
+	let binary ='';
+	while(num>0){
+		const rem = num%2;
+		binary += rem;
+		num = Math.floor(num/2);
+	}
+	return binary;
   
-// }
-	consol.log(decimalToBinary(num));
+}
 
-window.decimalToBinary = decimalToBinary;
+// window.decimalToBinary = decimalToBinary;
